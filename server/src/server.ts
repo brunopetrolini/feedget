a4489a80ac1ff45e005a6acf7f31abda3764adac
Bruno Petrolini <brunopetrolini@hotmail.com>
2022-05-04 17:14:42 -0300

    feat: add prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,2 +1,9 @@
-const test = 1;
-console.log(test);
+import express from 'express';
+
+import { version } from '../package.json';
+
+const app = express();
+
+app.get('/health', (request, response) => response.json({ version }));
+
+app.listen(4000, () => console.log('HTTP server running'));
