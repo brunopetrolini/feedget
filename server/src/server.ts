d1c435e8246c60b42293c2c3b3bb768a20513a02
Bruno Petrolini <brunopetrolini@hotmail.com>
2022-05-04 17:58:59 -0300

    feat: add email sender

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,17 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 import { version, name } from '../package.json';
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '7861e553e56170',
+    pass: 'f1ad6b5fb3b842',
+  },
+});
 
 const app = express();
 app.use(express.json());
@@ -11,6 +21,18 @@ app.post('/feedbacks', async (request, response) => {
 
   const feedback = await prisma.feedback.create({
     data: { type, comment, screenshot },
+  });
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Bruno Petrolini <brunopetrolini@hotmail.com>',
+    subject: 'Novo Feedback',
+    html: [
+      '<div style="font-family: sans-serif; font-size: 16px; color: #111;">',
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      '</div>',
+    ].join('\n'),
   });
 
   return response.status(201).json({ data: feedback });
