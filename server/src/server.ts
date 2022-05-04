a18126ab4d305fb1c4bc5eaee406d73945afd5b0
Bruno Petrolini <brunopetrolini@hotmail.com>
2022-05-04 17:44:34 -0300

    feat: add feedbacks creation route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import { prisma } from './prisma';
 
-import { version } from '../package.json';
+import { version, name } from '../package.json';
 
 const app = express();
+app.use(express.json());
 
-app.get('/health', (request, response) => response.json({ version }));
+app.post('/feedbacks', async (request, response) => {
+  const { type, comment, screenshot } = request.body;
+
+  const feedback = await prisma.feedback.create({
+    data: { type, comment, screenshot },
+  });
+
+  return response.status(201).json({ data: feedback });
+});
+
+app.get('/health', (request, response) => response.json({ name, version }));
 
 app.listen(4000, () => console.log('HTTP server running'));
