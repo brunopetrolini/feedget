535599699031942e19a40b1cb852628f0142861d
Bruno Petrolini <brunopetrolini@hotmail.com>
2022-05-07 12:14:32 -0300

    feat: add copyright component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,16 @@
-import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
 import AppLoading from 'expo-app-loading';
 import { View } from 'react-native';
 
-import { Widget } from './src/components/Widget';
+import { 
+  useFonts, 
+  Inter_400Regular, 
+  Inter_500Medium 
+} from '@expo-google-fonts/inter';
+
+import Widget from './src/components/Widget';
 import { theme } from './src/theme';
 
 export default function App() {
@@ -18,12 +25,12 @@ export default function App() {
       flex: 1,
       backgroundColor: theme.colors.background
     }}>
-      <Widget />
       <StatusBar 
         style="light"
         backgroundColor='transparent'
         translucent 
       />
+      <Widget />
     </View>
   );
 }
