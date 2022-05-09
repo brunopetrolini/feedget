a30122a0c43b97ca7059f2803194f2706c37c9b9
Bruno Petrolini <brunopetrolini@hotmail.com>
2022-05-09 13:39:59 -0300

    feat: add navigation and api communication

--- mobile/src/components/Form/index.tsx
+++ mobile/src/components/Form/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ArrowLeft } from 'phosphor-react-native';
 import { 
   View, 
@@ -14,18 +14,55 @@ import { styles } from './styles';
 import { theme } from '../../theme';
 import { feedbackTypes } from '../../utils/feedbackTypes';
 import { Button } from '../Button';
+import { captureScreen } from 'react-native-view-shot';
+import { api } from '../../libs/api';
 
 interface Props {
   feedbackType: FeedbackType;
+  onFeedbackCanceled: () => void;
+  onFeedbackSent: () => void;
 }
 
-export function Form({ feedbackType }: Props) {
+export function Form({ feedbackType, onFeedbackCanceled, onFeedbackSent }: Props) {
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [comment, setComment] = useState<string>('');
+  const [isSendingFeedback, setIsSendingFeedback] = useState<boolean>(false);
+
   const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  function handleTakeScreenshot() {
+    captureScreen({
+      format: 'jpg',
+      quality: 0.8
+    })
+    .then(uri => setScreenshot(uri))
+    .catch(error => console.log(error));
+  }
+
+  function handleRemoveScreenshot() {
+    setScreenshot(null);
+  }
+
+  async function handleSendFeedback() {
+    if (isSendingFeedback) return;
+    setIsSendingFeedback(true);
+
+    try {
+      await api.post('/feedbacks', {
+        type: feedbackType,
+        screenshot,
+        comment
+      });
+    } catch (error) {
+      setIsSendingFeedback(false);
+      console.log(error);
+    }
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={onFeedbackCanceled}>
           <ArrowLeft
             size={24}
             weight="bold"
@@ -44,15 +81,19 @@ export function Form({ feedbackType }: Props) {
         style={styles.input}
         placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..."
         placeholderTextColor={theme.colors.text_secondary}
+        onChangeText={setComment}
       />
 
       <View style={styles.footer}>
     	  <ScreenshotButton
-          onTakeShot={() => {}}
-          onRemoveShot={() => {}}
-          screenshot="http://github.com/brunopetrolini.png"
+          onTakeShot={handleTakeScreenshot}
+          onRemoveShot={handleRemoveScreenshot}
+          screenshot={screenshot}
         />
-        <Button isLoading={false} />
+        <Button 
+          onPress={handleSendFeedback}
+          isLoading={isSendingFeedback}
+        />
       </View>
     </View>
   );
